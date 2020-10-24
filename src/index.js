b8406afe2d6d2595f7db5afdb0f1948a2e2cff09
AnnaLisovskaya <60646192+AnnaLisovskaya@users.noreply.github.com>
2020-10-24 20:05:06 +0300

    init: true solution

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 module.exports = function reverse (n) {
-  if (n < 0) {
-        n = n*(-1);
-    } else if (n % 10 === 0) {
-        n = n / 10;
-    }
-    var i = n;
-    var res = 0;
-    while (i >= 0)  {
-        
-        if (i > 10) {
-            res = (i % 10 + res) * 10;
-        } else {
-            res = i % 10 + res;
-        }
-        i = Math.trunc(i / 10);
-    }
-    return res;
+  return (
+    parseFloat(
+      n
+        .toString()
+        .split('')
+        .reverse()
+        .join('')
+    ) 
+  )
 }
